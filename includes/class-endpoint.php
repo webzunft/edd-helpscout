8b6890492825ebaca77a38c81ed4ea6a9c386443
Danny van Kooten <dannyvankooten@gmail.com>
2014-06-16 16:33:38 +0200

    fix typo

--- includes/class-endpoint.php
+++ includes/class-endpoint.php
@@ -59,7 +59,7 @@ class EDD_HS_Endpoint {
 		$query   = "SELECT pm2.post_id, pm2.meta_value, p.post_status FROM $wpdb->postmeta pm, $wpdb->postmeta pm2, $wpdb->posts p WHERE pm.meta_key = '_edd_payment_user_email' AND pm.meta_value $email_query AND pm.post_id = pm2.post_id AND pm2.meta_key = '_edd_payment_meta' AND pm.post_id = p.ID ORDER BY pm.post_id DESC";
 		$results = $wpdb->get_results( $query );
 
-		if ( ! $results && ! empty( $data['customer']['fname'] ) && ! empty( $data['customer']['fname'] ) ) {
+		if ( ! $results && ! empty( $data['customer']['fname'] ) && ! empty( $data['customer']['lname'] ) ) {
 			// query by LIKE firstname AND LIKE lastname
 			$query   = "SELECT pm.post_id, pm.meta_value, p.post_status FROM $wpdb->postmeta pm, $wpdb->posts p WHERE pm.meta_key = '_edd_payment_meta' AND pm.meta_value LIKE '%%" . $data['customer']['fname'] . "%%' AND pm.meta_value LIKE '%%" . $data['customer']['lname'] . "%%' AND pm.post_id = p.ID ORDER BY pm.post_id DESC";
 			$results = $wpdb->get_results( $query );
