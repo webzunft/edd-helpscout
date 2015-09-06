52351b7b68a6f4a766be992a288d5d803e61b771
Danny van Kooten <dannyvankooten@gmail.com>
2015-09-06 17:15:42 +0700

    Add a bunch of action hooks for plugins to hook into. Closes #26

--- views/order-row.php
+++ views/order-row.php
@@ -1,6 +1,15 @@
 <?php defined( 'ABSPATH' ) or exit; ?>
 <div class="toggleGroup <?php if( $order['is_completed'] ) echo 'open'; ?>">
-	<strong><i class="icon-cart"></i> <?php echo $order['link']; ?></strong><a class="toggleBtn"><i class="icon-arrow"></i></a>
+
+	<?php do_action( 'edd_helpscout_before_order', $order ); ?>
+
+	<strong>
+		<i class="icon-cart"></i>
+		<a target="_blank" href="<?php echo esc_attr( admin_url( 'edit.php?post_type=download&page=edd-payment-history&view=view-order-details&id='. $order['payment_id'] ) ); ?>">#<?php echo $order['payment_id']; ?></a>
+	</strong>
+	<a class="toggleBtn"><i class="icon-arrow"></i></a>
+
+	<?php do_action( 'edd_helpscout_before_order_status', $order ); ?>
 
 	<?php if( $order['is_completed'] ) { ?>
 		<a style="float:right" href="<?php echo  esc_url( $order['resend_receipt_link'] ); ?>" target="_blank">
@@ -14,20 +23,32 @@
 		<span style="color:#008000;font-weight:bold;"> (renewal)</span>
 	<?php endif; ?>
 
+	<?php do_action( 'edd_helpscout_after_order_status', $order ); ?>
+
 	<div class="toggle indent">
+
+		<?php do_action( 'edd_helpscout_before_order_details', $order ); ?>
+
 		<p>
 			<span class="muted"><?php echo $order['date']; ?></span><br/>
 			<?php echo trim( edd_currency_filter( $order['amount'] ) ) . ( ( isset( $order['payment_method'] ) && '' !== $order['payment_method'] ) ?  ' - ' . $order['payment_method'] : '' ); ?>
 		</p>
 
 		<?php if ( ! empty( $order['downloads'] ) ) : ?>
+			<?php do_action( 'edd_helpscout_before_order_downloads', $order, $downloads ); ?>
+
 			<ul class="unstyled">
 				<?php foreach( $order['downloads'] as $download ) : ?>
 					<li>
 						<strong><?php echo get_the_title( $download['id'] ); ?></strong><br />
 						<?php echo edd_get_price_option_name( $download['id'], $download['options']['price_id'] ); ?>
 
+						<?php do_action( 'edd_helpscout_before_order_download_details', $order, $download ); ?>
+
 						<?php if( ! empty( $download['license'] ) ) : ?>
+
+							<?php do_action( 'edd_helpscout_before_order_download_license', $order, $download, $download['license'] ); ?>
+
 							<a href="<?php echo admin_url( 'edit.php?post_type=download&page=edd-licenses&s=' . $download['license']['key'] ); ?>">
 								<?php echo $download['license']['key']; ?>
 							</a>
@@ -45,18 +66,30 @@
 													<a href="<?php echo esc_url( $site['url'] ); ?>" target="_blank"><?php echo esc_html( ltrim( $site['url'], 'http://' ) ); ?></a>
 													<a href="<?php echo esc_url( $site['deactivate_link'] ); ?>" target="_blank"><small>(deactivate)</small></a>
 												</li>
-											<?php endforeach; ?>
+											<?php endforeach; // end foreach sites ?>
 										</ul>
 									</div>
 								</div>
-							<?php endif; ?>
-						<?php endif; ?>
+							<?php endif; // end if sites not empty ?>
+
+							<?php do_action( 'edd_helpscout_after_order_download_license', $order, $download, $download['license'] ); ?>
+
+						<?php endif; //end if has license ?>
+
+						<?php do_action( 'edd_helpscout_after_order_download_details', $order, $download ); ?>
 					</li>
 				<?php endforeach; ?>
 			</ul>
-		<?php endif; ?>
+
+			<?php do_action( 'edd_helpscout_after_order_downloads', $order, $downloads ); ?>
+
+		<?php endif; // endif downloads ?>
+
+		<?php do_action( 'edd_helpscout_after_order_details', $order ); ?>
 
 	</div>
+
+	<?php do_action( 'edd_helpscout_after_order', $order ); ?>
 
 </div>
 
