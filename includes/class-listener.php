8ef4537f017b5d09e1ff0900b4b41026ebd16eda
flowdee <mail@flowdee.de>
2020-11-18 11:56:49 +0100

    added multisite fix, when REQUEST_URI starts with the blog path #21

--- includes/class-listener.php
+++ includes/class-listener.php
@@ -23,9 +23,19 @@ class Listener {
 	 * @return boolean
 	 */
 	public function listen( $url ) {
-            
-                // warn if user still uses /edd-helpscout/api as prior v 2.0 is still used here
-                if( strpos( $url, '/edd-helpscout/api' ) === 0 ) {
+
+        // Multisite fix: In case the REQUEST_URI starts with the blog path in the URL (e.g. /de/edd-helpscout-api/customer_info) the URL would be declined later
+        if ( is_multisite() && isset ( $GLOBALS['blog_id'] ) ) {
+            $blog_details =  get_blog_details( $GLOBALS['blog_id'] );
+
+            // Removing the blog path from the beginning of the URL
+            if ( ! empty ( $blog_details->path ) && substr( $url, 0, strlen( $blog_details->path ) ) === $blog_details->path) {
+                $url = '/' . substr( $url, strlen( $blog_details->path ) );
+            }
+        }
+
+        // warn if user still uses /edd-helpscout/api as prior v 2.0 is still used here
+        if( strpos( $url, '/edd-helpscout/api' ) === 0 ) {
 			trigger_error( sprintf( __( '`%1$s` is deprecated since version %2$s! Follow the installation instructions and use `%3$s` in your Help Scount account instead.', 'edd-helpscout' ), '/edd-helpscout/api', '2.0', '/edd-helpscout-api/customer_info' ) );
 		}
 
@@ -35,11 +45,10 @@ class Listener {
 		}
 
 		// extract action argument
-
 		$url = substr( $url, strlen( $this->base_url ) );
 		$url = parse_url( $url, PHP_URL_PATH );
 		$pieces = explode( '/', $url );
-                
+
 		if( empty( $pieces[0] ) ) {
 			return '';
 		}
