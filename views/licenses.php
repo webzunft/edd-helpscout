24d7e5b71143c651f354d7a4cb4de8859c89a3dc
Terminator Barbapapa <Terminator-Barbapapa@users.noreply.github.com>
2021-01-27 20:16:52 +0100

    Style licenses template

--- views/licenses.php
+++ views/licenses.php
@@ -1,50 +1,51 @@
 <div class="c-sb-section c-sb-section--toggle nested open">
-	<div class="c-sb-section__title js-sb-toggle">
-		<i class="icon-folder icon-sb"></i> Licenses
-		<i class="caret sb-caret"></i>
+	<div class="c-sb-section__title js-sb-toggle" style="font-size: 15px; padding: 6px 0 10px 0;">
+		<i class="icon-folder icon-sb" style="font-size: 19px; margin-right: 3px;"></i>Licenses<i class="caret sb-caret" style="margin-top: 4px;"></i>
 	</div>
-	<div class="c-sb-section__body">
+	<div class="c-sb-section__body" style="padding-top: 8px;">
 		<?php foreach ($licenses as $license_id => $license): ?>
 			<ul class="c-sb-list c-sb-list--compact">
 				<?php if ( $license['show_activations'] ): ?>
-					<li class="c-sb-list-item" style="font-size: 80% !important;">
-						<span class="badge <?= $license['status_color'] ?>"><?= $license['status'] ?></span>&nbsp;
+					<li class="c-sb-list-item" style="font-size: 85%!important; font-style: italic;">
+						<span class="badge <?= $license['status_color'] ?>" style="font-size: 10px; line-height: 14px; padding: 2px 4px;"><?= $license['status'] ?></span>&nbsp;
 						<?= $license['is_expired'] ? __('Expired') : __('Expires'); ?>: <?= $license['expires'] ?>
 					</li>
 				<?php endif ?>
 
-				<li class="c-sb-list-item"><strong><?= $license['title'] ?></strong></li>
-				<li class="c-sb-list-item" style="font-size: 80% !important;">
-					<?php if (!empty($license['price_option'])): ?><?= $license['price_option'] ?> - <?php endif ?>
+				<li class="c-sb-list-item" style="font-size: 14px; padding: 6px 0 4px 0;"><strong><?= $license['title'] ?></strong></li>
+				<li class="c-sb-list-item" style="font-size: 85%!important;">
+					<?php if (!empty($license['price_option'])): ?>
+						<?= $license['price_option'] ?>
+						<a href="<?= $license['url']?>" target="_blank"><span class="icon-gear" style="font-size: 16px; margin-left: 4px;"></span></a>
+						<br>
+					<?php endif ?>
 					<?= $license['key'] ?>
-					<a href="<?= $license['url']?>" target="_blank"><span class="icon-gear"></span></a>
 				</li>
 			</ul>
 			<div style="margin-left: 18px">
 			<?php foreach ($license['children'] as $child_license_id => $child_license): ?>
 				<ul class="c-sb-list c-sb-list--compact">
 					<?php if ( $child_license['show_activations'] ): ?>
-						<li class="c-sb-list-item" style="font-size: 80% !important;">
-							<span class="badge <?= $child_license['status_color'] ?>"><?= $child_license['status'] ?></span>&nbsp;
+						<li class="c-sb-list-item" style="font-size: 85%!important; font-style: italic;">
+							<span class="badge <?= $child_license['status_color'] ?>" style="font-size: 10px; line-height: 14px; padding: 2px 4px;"><?= $child_license['status'] ?></span>&nbsp;
 							<?= $child_license['is_expired'] ? __('Expired') : __('Expires'); ?>: <?= $child_license['expires'] ?>
 						</li>
 					<?php endif ?>
-					<li class="c-sb-list-item"><strong><?= $child_license['title'] ?></strong></li>
-					<li class="c-sb-list-item" style="font-size: 80% !important;">
-						<?php if (!empty($child_license['price_option'])): ?><?= $child_license['price_option'] ?> - <?php endif ?>
+					<li class="c-sb-list-item" style="font-size: 12px; line-height: 16px; padding: 4px 10px 2px 0;"><strong><?= $child_license['title'] ?></strong></li>
+					<li class="c-sb-list-item" style="font-size: 80%!important;">
 						<?= $child_license['key'] ?>
-						<a href="<?= $child_license['url']?>" target="_blank"><span class="icon-gear"></span></a>
+						<a href="<?= $child_license['url']?>" target="_blank"><span class="icon-gear" style="top: 1px; margin-left: 4px;"></span></a>
 					</li>
 				</ul>
 				<?php if ( $child_license['show_activations'] ): ?>
-					<div class="c-sb-section c-sb-section--toggle nested">
-						<div class="c-sb-section__title js-sb-toggle">
+					<div class="c-sb-section c-sb-section--toggle nested" style="padding-bottom: 14px;">
+						<div class="c-sb-section__title js-sb-toggle" style="font-size: 12px; border: none;">
 							Active sites (<?= $child_license['activation_count'] ?>/<?= $child_license['limit'] ?>) <i class="caret sb-caret"></i>
 						</div>
 						<div class="c-sb-section__body">
 							<ul class="c-sb-list c-sb-list--compact">
 								<?php foreach ($child_license['sites'] as $site): ?>
-									<li class="c-sb-list-item--bullet">
+									<li class="c-sb-list-item--bullet" style="list-style-type: circle;">
 										<a class="c-sb-list-item__link t-tx-blue-500" href="https://<?= $site ?>"><?= $site ?></a>
 									</li>
 								<?php endforeach ?>
@@ -55,8 +56,8 @@
 			<?php endforeach ?>
 			</div>
 			<?php if (!empty($license['upgrades'])): ?>
-				<div class="c-sb-section c-sb-section--toggle nested">
-					<div class="c-sb-section__title js-sb-toggle">
+				<div class="c-sb-section c-sb-section--toggle nested" style="padding-bottom: 14px;">
+					<div class="c-sb-section__title js-sb-toggle" style="font-size: 12px; border: none;">
 						Upgrades <i class="caret sb-caret"></i>
 					</div>
 					<div class="c-sb-section__body">
@@ -78,14 +79,14 @@
 				</div>
 			<?php endif ?>
 			<?php if ( $license['show_activations'] ): ?>
-				<div class="c-sb-section c-sb-section--toggle nested">
-					<div class="c-sb-section__title js-sb-toggle">
+				<div class="c-sb-section c-sb-section--toggle nested" style="padding-bottom: 14px;">
+					<div class="c-sb-section__title js-sb-toggle" style="font-size: 12px; border: none;">
 						Active sites (<?= $license['activation_count'] ?>/<?= $license['limit'] ?>) <i class="caret sb-caret"></i>
 					</div>
 					<div class="c-sb-section__body">
 						<ul class="c-sb-list c-sb-list--compact">
 							<?php foreach ($license['sites'] as $site): ?>
-								<li class="c-sb-list-item--bullet">
+								<li class="c-sb-list-item--bullet" style="list-style-type: circle;">
 									<a class="c-sb-list-item__link t-tx-blue-500" href="https://<?= $site ?>"><?= $site ?></a>
 								</li>
 							<?php endforeach ?>
@@ -94,7 +95,7 @@
 				</div>
 			<?php endif ?>
 			<?php if ( $license['is_expired'] ): ?>
-				<div class="c-sb-section">
+				<div class="c-sb-section" style="padding-bottom: 14px;">
 					<a href="<?= $license['renewal_url'] ?>"><?= __('Renew') ?></a>
 				</div>
 			<?php endif ?>
