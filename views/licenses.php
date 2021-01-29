680c7135b9faa7552da7859bd0c7d6384f7482e3
Terminator Barbapapa <Terminator-Barbapapa@users.noreply.github.com>
2021-01-29 11:58:18 +0100

    Licenses template style bundles

--- views/licenses.php
+++ views/licenses.php
@@ -4,7 +4,7 @@
 	</div>
 	<div class="c-sb-section__body" style="padding-top: 8px;">
 		<?php foreach ($licenses as $license_id => $license): ?>
-			<ul class="c-sb-list c-sb-list--compact">
+			<ul class="c-sb-list c-sb-list--compact" style="padding: 1em 0;">
 				<?php if ( $license['show_activations'] ): ?>
 					<li class="c-sb-list-item" style="font-size: 85%!important; font-style: italic;">
 						<span class="badge <?= $license['status_color'] ?>" style="font-size: 10px; line-height: 14px; padding: 2px 4px;"><?= $license['status'] ?></span>&nbsp;
@@ -24,7 +24,7 @@
 			</ul>
 			<div style="margin-left: 18px">
 			<?php foreach ($license['children'] as $child_license_id => $child_license): ?>
-				<ul class="c-sb-list c-sb-list--compact">
+				<ul class="c-sb-list c-sb-list--compact" style="padding: 8px 0 4px 0;">
 					<?php if ( $child_license['show_activations'] ): ?>
 						<li class="c-sb-list-item" style="font-size: 85%!important; font-style: italic;">
 							<span class="badge <?= $child_license['status_color'] ?>" style="font-size: 10px; line-height: 14px; padding: 2px 4px;"><?= $child_license['status'] ?></span>&nbsp;
@@ -34,12 +34,12 @@
 					<li class="c-sb-list-item" style="font-size: 12px; line-height: 16px; padding: 4px 10px 2px 0;"><strong><?= $child_license['title'] ?></strong></li>
 					<li class="c-sb-list-item" style="font-size: 80%!important;">
 						<?= $child_license['key'] ?>
-						<a href="<?= $child_license['url']?>" target="_blank"><span class="icon-gear" style="top: 1px; margin-left: 4px;"></span></a>
+						<a href="<?= $child_license['url']?>" target="_blank"><span class="icon-gear" style="top: 1px; margin-left: 4px; font-size: 16px;"></span></a>
 					</li>
 				</ul>
 				<?php if ( $child_license['show_activations'] ): ?>
 					<div class="c-sb-section c-sb-section--toggle nested" style="padding-bottom: 10px;">
-						<div class="c-sb-section__title js-sb-toggle" style="font-size: 12px; border: none;">
+						<div class="c-sb-section__title js-sb-toggle" style="font-size: 12px; border: none; padding: 0;">
 							Active sites (<?= $child_license['activation_count'] ?>/<?= $child_license['limit'] ?>) <i class="caret sb-caret"></i>
 						</div>
 						<div class="c-sb-section__body">
@@ -57,7 +57,7 @@
 			</div>
 			<?php if (!empty($license['upgrades'])): ?>
 				<div class="c-sb-section c-sb-section--toggle nested">
-					<div class="c-sb-section__title js-sb-toggle" style="font-size: 12px; border: none;">
+					<div class="c-sb-section__title js-sb-toggle" style="font-size: 12px; border: none; padding: 0 0 4px 0;">
 						Upgrades <i class="caret sb-caret"></i>
 					</div>
 					<div class="c-sb-section__body">
@@ -80,7 +80,7 @@
 			<?php endif ?>
 			<?php if ( $license['show_activations'] ): ?>
 				<div class="c-sb-section c-sb-section--toggle nested">
-					<div class="c-sb-section__title js-sb-toggle" style="font-size: 12px; border: none;">
+					<div class="c-sb-section__title js-sb-toggle" style="font-size: 12px; border: none; padding: 0 0 4px 0;">
 						Active sites (<?= $license['activation_count'] ?>/<?= $license['limit'] ?>) <i class="caret sb-caret"></i>
 					</div>
 					<div class="c-sb-section__body">
@@ -99,7 +99,7 @@
 					<a href="<?= $license['renewal_url'] ?>"><?= __('Renew') ?></a>
 				</div>
 			<?php endif ?>
-			<div class="divider" style="padding-top: 10px;"></div>
+			<div class="divider" style="padding-top: 10px; border-bottom: 1px solid rgba(181,186,191,.2); margin-bottom: -1px;"></div>
 		<?php endforeach ?>
 	</div>
 </div>
