8171b964f98ecd1049ed05950681685dbf076387
Terminator Barbapapa <Terminator-Barbapapa@users.noreply.github.com>
2021-01-29 11:58:33 +0100

    Style section titles

--- views/subscriptions.php
+++ views/subscriptions.php
@@ -1,7 +1,6 @@
 <div class="c-sb-section c-sb-section--toggle">
-	<div class="c-sb-section__title js-sb-toggle">
-		<i class="icon-star icon-sb"></i> Subscriptions
-		<i class="caret sb-caret"></i>
+	<div class="c-sb-section__title js-sb-toggle" style="font-size: 15px; padding: 6px 0 10px 0;">
+		<i class="icon-star icon-sb" style="font-size: 19px; margin-right: 3px;"></i>Subscriptions<i class="caret sb-caret" style="margin-top: 4px;"></i>
 	</div>
 	<div class="c-sb-section__body">
 		<ul class="c-sb-list c-sb-list--compact">
