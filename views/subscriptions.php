eddda54e8693d3815bced5b9e598c5c837798274
Terminator Barbapapa <Terminator-Barbapapa@users.noreply.github.com>
2021-02-01 12:11:16 +0100

    Remove bullet class instead of adding inline CSS

--- views/subscriptions.php
+++ views/subscriptions.php
@@ -10,7 +10,7 @@
 					<span class="badge <?= $subscription['status_color'] ?>" style="font-size: 85%; padding: 3px 4px; margin: -1px 4px 0 0;"><?= $subscription['status_label'] ?></span> <a href="<?= $subscription['url'] ?>" title="<?= $subscription['status_label'] ?>">#<?= $subscription_id ?></a>
 					</span>
 				</li>
-				<li class="c-sb-list-item c-sb-list-item--bullet" style="list-style-type: none; font-size: 14px; padding: 4px 0 2px 0;">
+				<li class="c-sb-list-item" style="font-size: 14px; padding: 4px 0 2px 0;">
 					<strong><?= $subscription['title'] ?></strong>
 				</li>
 				<li class="c-sb-list-item" style="padding-bottom: 8px; margin-bottom: 8px; border-bottom: 1px solid rgba(193,203,212,.2);">
