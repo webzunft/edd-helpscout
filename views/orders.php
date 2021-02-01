c11c248c67aad20f3840c919bbc9026ddcb59c11
Ewout Fernhout <info@drwoo.nl>
2021-02-01 13:25:05 +0100

    show price option per order item instead of per order

--- views/orders.php
+++ views/orders.php
@@ -12,19 +12,21 @@
 					</span>
 				</li>
 				<?php foreach ($order['items'] as $item): ?>
-					<li class="c-sb-list-item c-sb-list-item--bullet" style="list-style-type: circle; list-style-position: outside; margin-left: 1.2em; font-size: 14px; padding: 4px 0 6px 0;">
-						<strong><?= $item['title'] ?></strong>
+					<li class="c-sb-list-item c-sb-list-item--bullet" style="list-style-type: circle; list-style-position: outside; margin-left: 1.2em;padding: 4px 0 6px 0;">
+						<span class="c-sb-list-item__label t-tx-charcoal-500" style="padding-bottom: 4px;">
+							<strong style="font-size: 14px;"><?= $item['title'] ?></strong>
+							<?php if (!empty($item['price_option'])): ?>
+								<span class="c-sb-list-item__text t-tx-charcoal-500" style="font-size: 11px;"><?= $item['price_option'] ?></span>
+							<?php endif ?>
+						</span>
 					</li>
 				<?php endforeach ?>
 				<li class="c-sb-list-item">
-					<span class="c-sb-list-item__label t-tx-charcoal-500" style="padding-bottom: 4px;">
-						<?php do_action( 'edd_helpscout_order_list_item_data_start', $order, $helpscout_data ); ?>
-						<span class="c-sb-list-item__text t-tx-charcoal-500" style="font-size: 11px;">
-							<?php if (!empty($item['price_option'])): ?><?= $item['price_option'] ?> - <?php endif ?>
-							<?= $order['payment_method'] ?><br><?= $order['date'] ?>
-						</span>
-						<?php do_action( 'edd_helpscout_order_list_item_data_end', $order, $helpscout_data ); ?>
+					<?php do_action( 'edd_helpscout_order_list_item_data_start', $order, $helpscout_data ); ?>
+					<span class="c-sb-list-item__text t-tx-charcoal-500" style="font-size: 11px;">
+					<?= $order['date'] ?> - <?= $order['payment_method'] ?>
 					</span>
+					<?php do_action( 'edd_helpscout_order_list_item_data_end', $order, $helpscout_data ); ?>
 				</li>
 				<?php do_action( 'edd_helpscout_after_order_list_item', $order, $helpscout_data ); ?>
 				<li role="separator" class="c-sb-list-divider" style="margin-bottom: -1px; padding-top: 16px;"></li>
